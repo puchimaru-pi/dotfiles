3f89ff23f82b82dc25205fd8235e3827b14d6019
pipopo <puchi.maru.milk@gmail.com>
2023-09-14 15:59:25 +0900

    rm an option

--- .config/nvim/lua/pluginconfig/alpha-nvim.lua
+++ .config/nvim/lua/pluginconfig/alpha-nvim.lua
@@ -40,7 +40,7 @@ dashboard.section.header.opts.hl = "Question"
 -- dashboard.section.header.val = vim.fn.readfile(vim.fn.expand("~/.config/nvim/lua/rc/files/dashboard_custom_header.txt"))
 dashboard.section.buttons.val = {
 	dashboard.button("s", " Open last session", ":PossessionLoadCurrent<CR>"),
-	dashboard.button("h", " Recently opened files", ":Telescope my_mru<CR>"),
+	--dashboard.button("h", " Recently opened files", ":Telescope my_mru<CR>"),
 	dashboard.button("f", " Find file", ":Telescope find_files<CR>"),
 	dashboard.button("e", " New file", ":enew<CR>"),
 	dashboard.button("b", " Jump to bookmarks", ":Telescope marks<CR>"),
