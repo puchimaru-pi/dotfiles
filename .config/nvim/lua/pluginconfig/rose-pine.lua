eab9346f4485f68bf6033ef8bff218a9d8e06151
pipopo <puchi.maru.milk@gmail.com>
2023-09-14 12:23:37 +0900

    telescopeを半透明に

--- .config/nvim/lua/pluginconfig/rose-pine.lua
+++ .config/nvim/lua/pluginconfig/rose-pine.lua
@@ -2,11 +2,11 @@ require('rose-pine').setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
 	variant = 'moon',
 	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
+	dark_variant = 'moon',
 	bold_vert_split = false,
 	dim_nc_background = false,
-	disable_background = false,
-	disable_float_background = false,
+	disable_background = true,
+	disable_float_background = true,
 	disable_italics = false,
 
 	--- @usage string hex value or named color from rosepinetheme.com/palette
