c50e6909c5683d3cd0b5b925b2877d8460e7b5a3
pipopo <puchi.maru.milk@gmail.com>
2023-12-03 20:13:53 +0900

    refactor: change to a clearer writing style

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,27 @@
 local wezterm = require 'wezterm'
 
-return {
-    window_decorations = 'RESIZE',
+local config = {}
 
-    hide_tab_bar_if_only_one_tab = true,
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
 
-    --IME使えるようにするぜい！
-    use_ime = true,
+--windows
+config.window_decorations = 'RESIZE'
+config.window_background_opacity = 0.75
+config.hide_tab_bar_if_only_one_tab = true
 
-    --フォントけいじゃ！
-    font_size = 16,
-    font = wezterm.font_with_fallback {
-        { family = 'Hack Nerd Font', weight = 'Medium' },
-        { family = 'HackGen Console NF', weight='Medium', stretch='Normal', style=Normal },
-    },
+--IME
+config.use_ime = true
 
-    window_background_opacity = 0.75,
-
-    color_scheme = 'rose-pine-dawn'
+--fonts
+config.font_size = 16
+config.font = wezterm.font_with_fallback {
+    {family = 'Hack Nerd Font', weight = 'Medium'},
+    {family = 'HackGen Console NF', weight = 'Medium', stretch = 'Normal', style = Normal},
 }
+
+--color_scheme
+config.color_scheme = 'rose-pine-dawn'
+
+return config
