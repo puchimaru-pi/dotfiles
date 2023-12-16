3d48d7d4f155653ba9a51a56e68694f36799fcd0
pipopo <puchi.maru.milk@gmail.com>
2023-12-16 11:40:25 +0900

    config keybinds for pane

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,11 +17,23 @@ config.use_ime = true
 -- fonts
 config.font_size = 16
 config.font = wezterm.font_with_fallback {
-    {family = 'Hack Nerd Font', weight = 'Medium'},
-    {family = 'HackGen Console NF', weight = 'Medium', stretch = 'Normal', style = Normal},
+    { family = 'Hack Nerd Font', weight = 'Medium' },
+    { family = 'HackGen Console NF', weight = 'Medium', stretch = 'Normal', style = Normal },
 }
 
 -- color_scheme
 config.color_scheme = 'rose-pine-dawn'
+
+-- keybinds
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    { key = '-', mods = 'LEADER',       action = wezterm.action { SplitVertical = { domain = 'CurrentPaneDomain' }}},
+    { key = '|', mods = 'LEADER|SHIFT', action = wezterm.action { SplitHorizontal = { domain = 'CurrentPaneDomain' }}},
+    { key = 'j', mods = 'LEADER',       action = wezterm.action { ActivatePaneDirection = 'Left' }},
+    { key = 'k', mods = 'LEADER',       action = wezterm.action { ActivatePaneDirection = 'Down' }},
+    { key = 'l', mods = 'LEADER',       action = wezterm.action { ActivatePaneDirection = 'Up' }},
+    { key = ';', mods = 'LEADER',       action = wezterm.action { ActivatePaneDirection = 'Right' }},
+    { key = 'x', mods = 'LEADER',       action = wezterm.action { CloseCurrentPane = { confirm = true }}},
+}
 
 return config
