d66659c0b7f93c742513de970596da5fa61feea8
pipopo <puchi.maru.milk@gmail.com>
2023-12-16 11:13:24 +0900

    んなー

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,22 +6,22 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
---windows
+-- windows
 config.window_decorations = 'RESIZE'
 config.window_background_opacity = 0.77
 config.hide_tab_bar_if_only_one_tab = true
 
---IME
+-- IME
 config.use_ime = true
 
---fonts
+-- fonts
 config.font_size = 16
 config.font = wezterm.font_with_fallback {
     {family = 'Hack Nerd Font', weight = 'Medium'},
     {family = 'HackGen Console NF', weight = 'Medium', stretch = 'Normal', style = Normal},
 }
 
---color_scheme
+-- color_scheme
 config.color_scheme = 'rose-pine-dawn'
 
 return config
