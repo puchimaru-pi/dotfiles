39973be8b72659804357f394b8b37753fb7208a2
pipopo <puchi.maru.milk@gmail.com>
2024-01-19 10:25:35 +0900

    add alias

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -2,6 +2,10 @@
 alias .. 'cd ..'
 alias ... 'cd ../..'
 
-alias ls 'ls -G --color'
-alias ll 'ls -lG --color'
-alias la 'ls -laG --color'
+# alias ls 'ls -G --color'
+# alias ll 'ls -lG --color'
+# alias la 'ls -laG --color'
+
+alias ls='eza --icons --git --time-style relative'
+alias ll='eza --icons --git --time-style relative -l'
+alias la='eza --icons --git --time-style relative -al'
